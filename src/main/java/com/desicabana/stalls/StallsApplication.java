a20cca1537b26711d66a04e0417f36a91da09ef7
abhineetsite <110209136+abhineetsite@users.noreply.github.com>
2023-11-02 22:23:08 -0500

    Initial commit
    Initial setup

---
+++ src/main/java/com/desicabana/stalls/StallsApplication.java
@@ -0,0 +1,13 @@
+package com.desicabana.stalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StallsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StallsApplication.class, args);
+	}
+
+}
