dc87dd7e2cab0e066a85d85e4a3194bf38b558af
abhineetsite <abhineetswain@outlook.com>
2023-12-02 18:08:43 -0600

    Role based Security SQL
    The `role` table would contain different roles like 'employee', 'customer', 'admin', etc. The `user_role` table would link users to their roles, allowing for a many-to-many relationship where a user can have multiple roles and a role can be assigned to multiple users.

--- src/main/java/com/desicabana/stalls/config/SecurityConfig.java
+++ src/main/java/com/desicabana/stalls/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.desicabana.stalls.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+//import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -5,6 +5,8 @@
 --`id_proof`, `profile_picture`, and `resume` are `VARCHAR(255)` fields, which can store the path to the respective files.
 
 --Postgres
+
+-- Employee table
 CREATE TABLE employee (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
@@ -18,8 +20,61 @@ CREATE TABLE employee (
     address TEXT NOT NULL,
     id_proof VARCHAR(255) NOT NULL,
     profile_picture VARCHAR(255),
-    resume VARCHAR(255)
+    resume VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES user (id)
 );
+
+-- Customer table
+/* CREATE TABLE customer (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    middle_name VARCHAR(255),
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    address TEXT NOT NULL,
+    profile_picture VARCHAR(255),
+    FOREIGN KEY (user_id) REFERENCES user (id)
+); */
+
+-- User table
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    --employee_id INT NOT NULL,
+    --FOREIGN KEY (employee_id) REFERENCES employee (id)
+);
+
+-- Indexes
+-- User
+CREATE INDEX idx_user_username ON user (username);
+
+-- role table
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+-- user_role table
+CREATE TABLE user_role (
+    user_id INT NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES user (id),
+    FOREIGN KEY (role_id) REFERENCES role (id)
+);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
